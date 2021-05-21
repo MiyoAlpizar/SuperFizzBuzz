35c440afa684b4dd6e0b717a2b6a9f613937c591
miyoalpizar <Miyo@DESKTOP-VOHF5HE>
2021-05-21 13:41:03 -0500

    Split Methods

--- SuperFizzBuzz.Advanced/Program.cs
+++ SuperFizzBuzz.Advanced/Program.cs
@@ -6,14 +6,23 @@ namespace SuperFizzBuzz.Advanced
 {
     class Program
     {
+        static readonly SuperFizzBuzz superFizzBuzz = new();
         static void Main(string[] _)
         {
-            SuperFizzBuzz superFizzBuzz = new();
+            ExampleA();
+            ExampleB();
+        }
 
+        static void ExampleA()
+        {
+            superFizzBuzz.SetDefaultFizzBuzz();
             Console.WriteLine("--- Example -12 to 145 ---");
             superFizzBuzz.FizzBuzz(-12, 145);
             Console.WriteLine("--- Example -12 to 145 ---\n");
+        }
 
+        static void ExampleB()
+        {
             List<FizzBuzz> fizzBuzzes = new()
             {
                 new FizzBuzz { MultiplesOf = 3, Token = "Fizz" },
@@ -22,7 +31,7 @@ namespace SuperFizzBuzz.Advanced
             };
 
             superFizzBuzz.SetFizzBuzzes(fizzBuzzes);
-            superFizzBuzz.FizzBuzz(1,1000);
+            superFizzBuzz.FizzBuzz(-3, 1000);
         }
     }
 }
